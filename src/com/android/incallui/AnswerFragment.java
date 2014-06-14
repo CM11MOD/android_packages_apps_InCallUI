3ed412e31acf31fe9858b8b4975ceaccd9322eab
Jorge Ruesga <jorge@ruesga.com>
2014-06-14 12:38:30 +0200

    incallui: make navigation bar translucent + other fixes
    * Make navigation bar translucent when available, minimizing thec bad
      effect of the black navigation bar.
    * Complete change Ie76cbc53c176d94bfb2402bf50eae6161fbb2198, displaying
      the caller photo in complete full window size.
    * Sync Msim layouts with normal layouts
    
    Patchset 3: Use full translucent bars (status + navigation)
    Patchset 4: Revert layouts
    Patchset 5: Simplification, use transparent status bar during call
    Patchset 6: Fix dialpad layout
    Patchset 7: update translucency flags when hiding conference manager
    Patchset 8: also make status bar translucent when dialpad is shown
    
    Change-Id: I3a9e4e248f19b951dd6a127b6f63a0cd62047497
    JIRA: CYAN-4048
    Issue: https://jira.cyanogenmod.org/browse/CYAN-4048
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Conflicts:
    	res/layout/incall_screen.xml

--- src/com/android/incallui/AnswerFragment.java
+++ src/com/android/incallui/AnswerFragment.java
@@ -25,6 +25,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -99,14 +100,17 @@ public class AnswerFragment extends BaseFragment<AnswerPresenter, AnswerPresente
 
     @Override
     public void showAnswerUi(boolean show) {
+        final Window window = getActivity().getWindow();
         getView().setVisibility(show ? View.VISIBLE : View.GONE);
         getView().setBackgroundColor(0);
 
         Log.d(this, "Show answer UI: " + show);
         if (show) {
             mGlowpad.startPing();
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         } else {
             mGlowpad.stopPing();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
     }
 
