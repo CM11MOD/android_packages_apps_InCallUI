95bec8bca75506f8d877bab8b163de70076ffcba
emancebo <emancebo@cyngn.com>
2014-08-04 12:03:34 -0700

    Add an option in the InCall UI to perform call recording
    Change-Id: Icc86a0a7ae9d2493a837d02f321df7cb1387cded
    
    Conflicts:
    	res/layout/call_button_fragment.xml
    	res/layout/primary_call_info.xml
    	res/values/colors.xml
    	res/values/dimens.xml
    	src/com/android/incallui/CallButtonFragment.java
    	src/com/android/incallui/CallCardFragment.java
    	src/com/android/incallui/CallList.java

--- src/com/android/incallui/CallCardFragment.java
+++ src/com/android/incallui/CallCardFragment.java
@@ -19,12 +19,13 @@ package com.android.incallui;
 import android.animation.LayoutTransition;
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.mokee.location.PhoneLocation;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.text.format.DateUtils;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -63,6 +64,7 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
     private TextView mOrganization;
     private TextView mPosition;
     private TextView mCity;
+    private TextView mCallRecordingTimer;
 
     // Secondary caller info
     private ViewStub mSecondaryCallInfo;
@@ -72,6 +74,29 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
 
     // Cached DisplayMetrics density.
     private float mDensity;
+
+    private CallRecorder.RecordingProgressListener mRecordingProgressListener =
+            new CallRecorder.RecordingProgressListener() {
+        @Override
+        public void onStartRecording() {
+            mCallRecordingTimer.setText(DateUtils.formatElapsedTime(0));
+            mCallRecordingTimer.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onStopRecording() {
+            mCallRecordingTimer.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onRecordingTimeProgress(final long elapsedTimeMs) {
+            long elapsedSeconds = (elapsedTimeMs + 500) / 1000;
+            mCallRecordingTimer.setText(DateUtils.formatElapsedTime(elapsedSeconds));
+
+            // make sure this is visible in case we re-loaded the UI for a call in progress
+            mCallRecordingTimer.setVisibility(View.VISIBLE);
+        }
+    };
 
     /**
      * A subclass of ImageView which allows animation by LayoutTransition
@@ -152,16 +177,29 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
         mProviderNumber = (TextView) view.findViewById(R.id.providerAddress);
         mSupplementaryInfoContainer =
             (ViewGroup) view.findViewById(R.id.supplementary_info_container);
-        ViewGroup photoContainer = (ViewGroup) view.findViewById(R.id.photo_container);
-        LayoutTransition transition = photoContainer.getLayoutTransition();
-        transition.enableTransitionType(LayoutTransition.CHANGING);
-        transition.setAnimateParentHierarchy(false);
-        transition.setDuration(200);
         mDetailedCallInfo = view.findViewById(R.id.detailedCallInfo);
         mNickName = (TextView) view.findViewById(R.id.nickName);
         mPosition = (TextView) view.findViewById(R.id.position);
         mOrganization = (TextView) view.findViewById(R.id.organization);
         mCity = (TextView) view.findViewById(R.id.city);
+        mCallRecordingTimer = (TextView) view.findViewById(R.id.callRecordingTimer);
+
+        CallRecorder recorder = CallRecorder.getInstance();
+        recorder.addRecordingProgressListener(mRecordingProgressListener);
+
+        ViewGroup photoContainer = (ViewGroup) view.findViewById(R.id.photo_container);
+        LayoutTransition transition = photoContainer.getLayoutTransition();
+        transition.enableTransitionType(LayoutTransition.CHANGING);
+        transition.setAnimateParentHierarchy(false);
+        transition.setDuration(200);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        CallRecorder recorder = CallRecorder.getInstance();
+        recorder.removeRecordingProgressListener(mRecordingProgressListener);
     }
 
     @Override
